645bf3f5765d8ec659e87dd7b5e02018216626b6
mauryahimanshu517 <mauryahimanshu517@gmail.com>
2023-08-02 21:17:43 +0530

    finalupload

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function valuesetter(){
-    gsap.set("#nav",{y:"-100%",opacity:0})
+    gsap.set("#nav",{y:"0%",opacity:1})
     gsap.set("#home span .child",{y:"100%"})
     gsap.set("#home .row img",{opacity:0})
 
     document.querySelectorAll("#Visual>g").forEach(function(e) {
         var character = e.childNodes[0].childNodes[0];
-        character.style.strokeDasharray = character.getTotalLength() + 'px'; 
-        character.style.strokeOffset = character.getTotalLength() + 'px'; 
     });
 }
+
 function revealtospan(){
     document.querySelectorAll(".reveal")
 .forEach(function(elem){
@@ -104,6 +103,28 @@ function animatehomepage(){
     
 }
 
+function cardshovereffect(){
+document.querySelectorAll(".cnt").forEach(function(cnt){
+    var showingimage;
+    cnt.addEventListener("mousemove",function(dets){
+        console.log(dets.target.dataset.index);
+        console.log(document.querySelector("#cursor").children[dets.target]);
+        
+        document.querySelector("#cursor").children[dets.target.dataset.index].style.opacity=1;
+        showingimage=dets.target
+        document.querySelector("#cursor").children[dets.target.dataset.index].style.transform=`translate(${dets.clientX}px,${dets.clientY}px)`;
+        showingimage.style.filter="grayscale(1)";
+        document.querySelector("#work").style.backgroundColor="#"+dets.target.dataset.color;
+    })
+    cnt.addEventListener("mouseleave",function(dets){
+        
+        document.querySelector("#cursor").children[showingimage.dataset.index].style.opacity=0;
+        showingimage.style.filter="grayscale(0)"
+        document.querySelector("#work").style.backgroundColor="#fff ";
+
+    })
+})
+}
 function loco(){
     const scroll = new LocomotiveScroll({
         el: document.querySelector('#main'),
@@ -111,11 +132,69 @@ function loco(){
     });
 }
 
+function card(){
+    
+    var cardElement=document.querySelector(".sidebar");
+    var cross=document.querySelector(".cross")
+    var social=document.querySelector(".media")
+    var aboutdiv=document.querySelector("body")
+    var touch=document.querySelector(".Touch")
 
+    social.addEventListener("click",function(){
+        
+        cardElement.style.left="0%";
+        aboutdiv.style.overflowY = 'hidden';
+        touch.style.pointerEvents = "none";
+
+    })
+    cross.addEventListener('click',function(){
+        cardElement.style.left="-100%";
+        aboutdiv.style.overflowY = 'initial';
+        touch.style.pointerEvents = "auto";
+        
+    })
+}
+
+function getintouch(){
+    var gettouch=document.querySelector(".getintouch")
+    var getintouchs=document.querySelector("#getintouchs")
+    var touch=document.querySelector(".Touch")
+    var btn=document.querySelector(".btn")
+    var aboutdiv=document.querySelector("body")
+    var social=document.querySelector(".media")
+
+    touch.addEventListener("click",function(){
+       gettouch.style.right="10%"; 
+       getintouchs.style.right="-30%"
+       aboutdiv.style.overflowY = 'hidden';
+       social.style.pointerEvents = "none";
+    })
+    btn.addEventListener("click",function(event){
+      gettouch.style.right="-150%"
+      aboutdiv.style.overflowY = "initial";
+      social.style.pointerEvents = "auto";
+        
+    })
+}
+
+let linkClicked = false;
+
+        // Function to handle the link click and scrolling to the "About" section
+        function scrollToAbout() {
+            if (!linkClicked) {
+                // Scroll to the "About" section on the first click
+                document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+                linkClicked = true;
+            } else {
+                // Refresh the page on subsequent clicks
+                location.reload();
+            }
+        }
+// loco();
 revealtospan();
 valuesetter();
 loaderAnimation();
-loco()
-
-
+cardshovereffect();
+card();
+getintouch();
 
